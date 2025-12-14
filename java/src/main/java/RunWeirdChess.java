7d78b9a998fe5f0de75dcd6bd4a9df83c51a5c3b
sheptrundle <dvf5rd@virginia.edu>
2025-12-14 15:51:16 -0500

    Added starting code to RunWeirdChess

--- java/src/main/java/RunWeirdChess.java
+++ java/src/main/java/RunWeirdChess.java
@@ -1,5 +1,26 @@
-public class RunWeirdChess {
-    public static void main(String[] args) {
+import Controllers.StartScreenController;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
+import java.io.IOException;
+
+public class RunWeirdChess extends Application {
+    public static void main(String[] args) throws IOException {
+        launch(args);
+    }
+
+    public void start(Stage stage) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/FX/start-screen.fxml"));
+        Parent root = loader.load();
+        StartScreenController controller = loader.getController();
+
+        // Set scene
+        Scene scene = new Scene(root);
+        stage.setTitle("Eastern Roads");
+        stage.setScene(scene);
+        stage.show();
     }
 }
