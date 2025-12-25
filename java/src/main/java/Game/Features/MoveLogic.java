b9d9bb2dfab429c40de2666810ddcb48a72dd450
sheptrundle <dvf5rd@virginia.edu>
2025-12-25 18:36:11 -0500

    Added Bishop, Rook, and Queen moves

--- java/src/main/java/Game/Features/MoveLogic.java
+++ java/src/main/java/Game/Features/MoveLogic.java
@@ -1,6 +1,9 @@
 package Game.Features;
-
+import Game.Pieces.Bishop;
 import Game.Pieces.Piece;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MoveLogic {
 
@@ -20,6 +23,63 @@ public class MoveLogic {
 
         // Cannot move to position of own teams piece, or anything off board
         return false;
+    }
+
+    // Return all valid moves for a Bishop at a given position on a given board
+    public List<Position> bishopMoveSet(Piece piece, Position position, ChessBoard board) {
+        int[] dy = {1, -1};
+        int[] dx = {1, -1};
+        List<Position> validMoves = new ArrayList<>();
+        MoveLogic moveLogic = new MoveLogic();
+
+        // Expand diagonals
+        for (int y : dy) {
+            for (int x : dx) {
+                int row = position.getRow() + y;
+                int col = position.getColumn() + x;
+                while (moveLogic.isValidMove(piece, board, new Position(row, col))) {
+                    validMoves.add(new Position(row, col));
+                    row += y;
+                    col += x;
+                }
+            }
+        }
+        return validMoves;
+    }
+
+    // Return all valid moves for a Rook at a given position on a given board
+    public List<Position> rookMoveset(Piece piece, Position position, ChessBoard board) {
+        int[] dxy = {1, -1};
+        List<Position> validMoves = new ArrayList<>();
+        MoveLogic moveLogic = new MoveLogic();
+
+        // Expand left/right
+        for (int dx : dxy) {
+            int col = position.getColumn() + dx;
+            int row = position.getRow();
+            while (moveLogic.isValidMove(piece, board, new Position(row, col))) {
+                validMoves.add(new Position(row, col));
+                col += dx;
+            }
+        }
+
+        // Expand up/down
+        for (int dy : dxy) {
+            int row = position.getRow() + dy;
+            int col = position.getColumn();
+            while (moveLogic.isValidMove(piece, board, new Position(row, col))) {
+                validMoves.add(new Position(row, col));
+                row += dy;
+            }
+        }
+        return validMoves;
+    }
+
+    public List<Position> queenMoveset(Piece piece, Position position, ChessBoard board) {
+        List<Position> queenMoves = new ArrayList<>();
+        queenMoves.addAll(rookMoveset(piece, position, board));
+        queenMoves.addAll(bishopMoveSet(piece, position, board));
+        return queenMoves;
     }
 
     // Todo: implement these
